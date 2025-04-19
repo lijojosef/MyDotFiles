c93ecc2e57478e492f0f769ab2798dc5bace48f8
Lijo Joseph <lijojoseph234@gmail.com>
2025-04-19 19:50:58 +0530

    Adding some colorschemes
    Added some colorschemes and a way to set the default colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "folke/tokyonight.nvim", name = "tokyonight" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "shaunsingh/nord.nvim", name = "nord" },
+  { "EdenEast/nightfox.nvim", name = "nightfox" },
+
+  -- default colorscheme
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}

19dae001fffd6739a21a4ad4f76e82e6f16cb966
Lijo Joseph <lijojoseph234@gmail.com>
2026-02-08 07:18:23 +0530

    init support for basic motion and basic command (supports vscode setup as well)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,37 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- <leader> key
+vim.g.mapleader = ' '
+
+-- open config
+vim.cmd('nmap <leader>c :e ~/.config/nvim/init.lua<cr>')
+
+-- save
+vim.cmd('nmap <leader>s :w<cr>')
+
+
+-- repeat previous f, t, F or T movement
+vim.keymap.set('n', '\'', ';')
+
+-- paste without overwriting
+vim.keymap.set('v', 'p', 'P')
+
+-- redo
+vim.keymap.set('n', 'U', '<C-r>')
+
+-- clear search highlighting
+vim.keymap.set('n', '<Esc>', ':nohlsearch<cr>')
+
+-- skip folds (down, up)
+vim.cmd('nmap k gj')
+vim.cmd('nmap l gk')
+
+-- sync system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- search ignoring case
+vim.opt.ignorecase = true
+
+-- disable "ignorecase" option if the search pattern contains upper case characters
+vim.opt.smartcase = true
